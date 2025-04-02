8d30012765044a562aa568911bf4aeafb2633a0f
kelvinuwomano <jameskelvincruz69@gmail.com>
2025-04-02 12:34:29 +0100

    fix appointmen

--- routes/patientRoute.js
+++ routes/patientRoute.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const { signUp, verificationEmail, login, forgotPassword, resetPassword, getAllPharmacy } = require("../controllers/patientController");
 const { bookAppointment, getAllAppointment, cancelAppointment } = require("../controllers/appointmentController");
+const { protect } = require("../middleware/authMiddleware");
 // const { getNearbyPharmacy } = require("../controllers/pharmacyController");
 
 
@@ -14,7 +15,7 @@ patientRouter.post("/login", login)
 patientRouter.post("/forgot-password", forgotPassword)
 patientRouter.post("/reset-password/:token", resetPassword)
 patientRouter.get("/all-pharmacy", getAllPharmacy)
-patientRouter.post("/book-appointment", bookAppointment)
+patientRouter.post("/book-appointment", protect, bookAppointment)
 patientRouter.get("/all-appointments/:userId", getAllAppointment)
 patientRouter.delete("/cancel-appointment/:id", cancelAppointment)
 // patientRouter.get("/for-you", getNearbyPharmacy)
